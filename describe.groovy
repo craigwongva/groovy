c985d8f3a3b5e31f2720f0d708fd7e226a3b4a16
EC2 Default User <ec2-user@ip-172-31-35-101.us-west-2.compute.internal>
2017-02-10 19:21:53 +0000

    Stop using crontab cuz fails method def

--- describe.groovy
+++ describe.groovy
@@ -5,16 +5,20 @@ import groovyx.net.http.*
 
 class Hello {
 
- void capture() {
+ void capture(String region) {
   def s = "aws ec2 describe-instances --region us-west-2".execute().text
   def timestamp = 'date +"%Y_%m_%d_%H_%M_%S"'.execute().text
    .replaceAll('"','')
    .replaceAll('\n','')
 
-  def f = new File("captured/${timestamp}.json")
+  def f = new File("captured/$region/${timestamp}.json")
   f.write(s)
  }
 }
 
 def h = new Hello()
-h.capture()
+while (1 > 0) {
+ h.capture('us-west-2')
+ h.capture('us-east-1')
+ sleep 1000*(1*60)
+}
