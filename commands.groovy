72de1c11beaf5593f1977547258bf7f678bb2df5
EC2 Default User <ec2-user@ip-172-31-45-42.us-west-2.compute.internal>
2017-03-03 14:28:19 +0000

    Print friendlier SQL

--- commands.groovy
+++ commands.groovy
@@ -20,193 +20,6 @@ class Hello {
   jsonSlurper = new JsonSlurper()
  }
 
- ArrayList getNamesOfCapturedFiles(dirname) {
-  //dirname is a directory with subdirectories
-  //the subdirectories are 'us-east-1' and 'us-west-2'
-  def files = []
-  def dir = new File(dirname)
-  dir.eachFileRecurse (FileType.FILES) { file ->
-   files << file
-  }
-  def sortedfiles = files.sort()
- }
-
- def interpretNameOfCapturedFile(dirname, s) {
-   def secondsInPreviousMonthsIn2017 = [
-    '01':  0*(24*60*60),
-    '02': 31*(24*60*60),
-    '03': 59*(24*60*60),
-    '04': 90*(24*60*60),
-    '05':120*(24*60*60),
-    '06':151*(24*60*60),
-    '07':181*(24*60*60),
-    '08':212*(24*60*60),
-    '09':243*(24*60*60),
-    '10':273*(24*60*60),
-    '11':304*(24*60*60),
-    '12':334*(24*60*60)
-   ]
-
-   def sskey = s.toString()
-   def region = sskey["$dirname/".size().."$dirname/us-east-1".size()-1] 
-   def timestamp = sskey[
-    "$dirname/us-east-1/".size()..
-    "$dirname/us-east-1/".size()+
-     '2017_02_10_08_01_01'.size()-1]
-   def timestampYear = timestamp[0..3]
-   def timestampMonth = timestamp[5..6]
-   def timestampDay = timestamp[8..9]
-   def timestampHour = timestamp[11..12]
-   def timestampMinute = timestamp[14..15]
-   def timestampSecond = timestamp[17..18]
-
-   def timestampSecondsInto2017 = 
-    secondsInPreviousMonthsIn2017[timestampMonth]+
-    timestampDay.toInteger()*24*60*60 + 
-    timestampHour.toInteger()*60*60 + 
-    timestampMinute.toInteger()*60 + 
-    timestampSecond.toInteger()
-   [
-    region: region,
-    timestamp: timestamp,
-    timestampYear: timestampYear,
-    timestampMonth: timestampMonth,
-    timestampDay: timestampDay,
-    timestampHour: timestampHour,
-    timestampMinute: timestampMinute,
-    timestampSecond: timestampSecond,
-    timestampSecondsInto2017: timestampSecondsInto2017
-   ]
- }
-
- void interpretEC2jsonAndPrint(f, t) {
-   //The interpreted json values are printed here as a convenience
-   // rather than returning them within an array.
-   def object = jsonSlurper.parseText(t) 
-   for (def i = 0; i < object.Reservations.size(); i++) {
-    def instanceId   = object.Reservations[i].Instances[0].InstanceId
-    def instanceType = object.Reservations[i].Instances[0].InstanceType
-    def keyName      = object.Reservations[i].Instances[0].KeyName
-    def state        = object.Reservations[i].Instances[0].State.Name
-    def projectValue = getTagValue(object.Reservations[i].Instances[0].Tags, 'Project')
-    def nameValue    = getTagValue(object.Reservations[i].Instances[0].Tags, 'Name')
-
-    print   "${f.region},${f.timestamp},${f.timestampSecondsInto2017},"
-    print   "${f.timestampYear},${f.timestampMonth},${f.timestampDay},"
-    print   "${f.timestampHour},${f.timestampMinute},${f.timestampSecond},"
-    println "$instanceId,$instanceType,$keyName,$state,$projectValue,$nameValue"
-   }
- }
-
- void step1() {
-  def sortedfiles = getNamesOfCapturedFiles('captured')
-
-  sortedfiles.each { s ->
-   HashMap f = interpretNameOfCapturedFile('captured', s)
-   String t = new File(s.toString()).text
-   interpretEC2jsonAndPrint(f, t)
-  }
- }
-
- String getTagValue(tags, tag) {
-  def tagvalue = "no${tag}tag"
-
-  if (tags) {
-   for (int i=0; i<tags.size(); i++) {
-    if (tags[i].Key == tag) {
-     tagvalue = tags[i].Value
-    }
-   }
-  }
-
-  tagvalue
- }
-
- int secondsInto2017(timestamp) {
-   def NUM_SECONDS_IN_A_DAY = 24*60*60
-
-   if (timestamp == 'n/a') {
-    return -1
-   }
-
-   def timestampYear = timestamp[0..3]
-   def timestampMonth = timestamp[5..6]
-   def timestampDay = timestamp[8..9]
-   def timestampHour = timestamp[11..12]
-   def timestampMinute = timestamp[14..15]
-   def timestampSecond = timestamp[17..18]
-   def secondsInPreviousMonthsIn2017 = [
-    '01':  0*NUM_SECONDS_IN_A_DAY,
-    '02': 31*NUM_SECONDS_IN_A_DAY,
-    '03': 59*NUM_SECONDS_IN_A_DAY,
-    '04': 90*NUM_SECONDS_IN_A_DAY,
-    '05':120*NUM_SECONDS_IN_A_DAY,
-    '06':151*NUM_SECONDS_IN_A_DAY,
-    '07':181*NUM_SECONDS_IN_A_DAY,
-    '08':212*NUM_SECONDS_IN_A_DAY,
-    '09':243*NUM_SECONDS_IN_A_DAY,
-    '10':273*NUM_SECONDS_IN_A_DAY,
-    '11':304*NUM_SECONDS_IN_A_DAY,
-    '12':334*NUM_SECONDS_IN_A_DAY
-   ]
-   def timestampSecondsInto2017 = 
-    timestampDay.toInteger()*NUM_SECONDS_IN_A_DAY + 
-    timestampHour.toInteger()*60*60 + 
-    timestampMinute.toInteger()*60 + 
-    timestampSecond.toInteger()
- }
-
- void step2(inputfilename) {
-  Class.forName("org.h2.Driver");
-  Connection conn = DriverManager.
-  //getConnection("jdbc:h2:~/cat", "sa", "");
-  getConnection("jdbc:h2:tcp://localhost/~/cat", "sa", "");
-
-  def stmt2 = conn.createStatement()
-  try {
-   String sql2 = "drop table cat.public.step1"
-   def x2 = stmt2.execute(sql2)
-  }
-  catch (e) {
-  }
-
-  def stmt3 = conn.createStatement()
-  String sql3 = '' +
-   'create table cat.public.step1 (' +
-   ' region varchar(100),' +
-   ' xtimestamp varchar(100),' + 
-   ' xseconds2017 varchar(100),' +
-   ' xyear varchar(100),' +
-   ' xmonth varchar(100),' +
-   ' xday varchar(100),' +
-   ' xhour varchar(100),' +
-   ' xminute varchar(100),' +
-   ' xsecond varchar(100),' +
-   ' instanceid varchar(100),' +
-   ' type varchar(100),' +
-   ' secgrp varchar(100),' +
-   ' status varchar(100),' +
-   ' projectValue varchar(100),' +
-   ' nameValue varchar(100))' +
-   " as select * from CSVREAD('deleteme4')"
-   .replaceAll('deleteme4',inputfilename)
-
-   def x3 = stmt3.execute(sql3)
-
-  def stmt4 = conn.createStatement();
-  String sql4 = "select * from step1 order by instanceid, xtimestamp"
-  ResultSet rs4 = stmt4.executeQuery(sql4);
-
-  def i=0
-  while(rs4.next() && i++ <= 2000000) {
-   String region       = rs4.getString("region");
-
-   print   "$region,$instanceid,$xtimestamp,$savepreviousxtimestamp,"
-  }
-
-  conn.close();
- }
-
  void step11(sentence) {
   String sql1 = "insert into sentences(s) values('$sentence')"
   def stmt1 = conn.prepareStatement(sql1, Statement.RETURN_GENERATED_KEYS)
@@ -357,27 +170,27 @@ class Hello {
   }
  }
 
- int step18(int idOfSentenceJustSeen, String regexp) {
+ int step18(boolean printSQL, int idOfSentenceJustSeen, String regexp) {
   def stmt4 = conn.createStatement();
   String sql4 = ''
    //18: select S where s and r limit 1
    //Not an optimized query!
-   sql4 += 'select * from ( '
-   sql4 += 'select rownum r, s.id ids, s.s, l.id idl, l.sid, l.l '
-   sql4 += 'from sentences s '
-   sql4 += 'join labels l '
-   sql4 += 'on s.id = l.sid '
-   sql4 += "where s.id <> $idOfSentenceJustSeen "
-   sql4 += "and s.s regexp '$regexp' "
-   sql4 += 'and l in ( '
-   sql4 += 'select l.l '
-   sql4 += 'from labels l '
-   sql4 += "where sid = $idOfSentenceJustSeen "
-   sql4 += ') '
-   sql4 += ') '
-   sql4 += 'order by rand() '
-   sql4 += 'limit 1 '
-
+   sql4 += 'select * from (\n'
+   sql4 += 'select rownum r, s.id ids, s.s, l.id idl, l.sid, l.l\n'
+   sql4 += 'from sentences s\n'
+   sql4 += 'join labels l\n'
+   sql4 += 'on s.id = l.sid\n'
+   sql4 += "where s.id <> $idOfSentenceJustSeen\n"
+   sql4 += "and s.s regexp '$regexp'\n"
+   sql4 += 'and l in (\n'
+   sql4 += 'select l.l\n'
+   sql4 += 'from labels l\n'
+   sql4 += "where sid = $idOfSentenceJustSeen\n"
+   sql4 += ')\n'
+   sql4 += ')\n'
+   sql4 += 'order by rand()\n'
+   sql4 += 'limit 1\n'
+  if (printSQL) println sql4
   ResultSet rs4 = stmt4.executeQuery(sql4);
 
   String r
@@ -422,22 +235,23 @@ class Hello {
   }
  }
 
- int step16b(int idOfSentenceJustSeen) {
+ int step24(int idOfSentenceJustSeen) {
   def stmt4 = conn.createStatement();
   String sql4 = ''
    //select S where s limit 1
-   sql4 += 'select s.id ids, s.s, l.id idl, l.sid, l.l '
-   sql4 += 'from sentences s '
-   sql4 += 'join labels l '
-   sql4 += 'on s.id = l.sid '
-   sql4 += "where s.id <> $idOfSentenceJustSeen "
-   sql4 += 'and l in ( '
-   sql4 += 'select l.l '
-   sql4 += 'from labels l '
-   sql4 += "where sid = $idOfSentenceJustSeen "
-   sql4 += ') '
-   sql4 += 'limit 1 '
-
+   sql4 += 'select s.id ids, s.s, l.id idl, l.sid, l.l\n'
+   sql4 += 'from sentences s\n'
+   sql4 += 'join labels l\n'
+   sql4 += 'on s.id = l.sid\n'
+   sql4 += "where s.id <> $idOfSentenceJustSeen\n"
+   sql4 += 'and l in (\n'
+   sql4 += 'select l.l\n'
+   sql4 += 'from labels l\n'
+   sql4 += "where sid = $idOfSentenceJustSeen\n"
+   sql4 += ')\n'
+   sql4 += 'order by rand() '
+   sql4 += 'limit 1\n'
+  println sql4
   ResultSet rs4 = stmt4.executeQuery(sql4);
 
   int ids = -1
@@ -466,7 +280,7 @@ class Hello {
    sql4 += 'from labels l '
    sql4 += "where sid = $idOfSentenceJustSeen "
    sql4 += ') '
-
+  println sql4
   ResultSet rs4 = stmt4.executeQuery(sql4);
 
   def i=0
@@ -540,7 +354,7 @@ class Hello {
    sql4 += 'join labels l '
    sql4 += 'on s.id = l.sid '
    sql4 += "where l = '$hyphenedLabel' "
-
+  println sql4
   ResultSet rs4 = stmt4.executeQuery(sql4);
 
   def i=0
@@ -554,7 +368,7 @@ class Hello {
 }
 
 def h = new Hello()
-def idOfSentenceJustSeen = 34
+def idOfSentenceJustSeen = 87
 System.in.eachLine() { line ->  
  def argx = line.split(':')
 
@@ -632,9 +446,9 @@ if ((argx[0]) == '15') {
  h.step15(idOfSentenceJustSeen)
 }
 
-if ((argx[0]) == '16b') {
- println "16b: select S where s limit 1"
- def temp = h.step16b(idOfSentenceJustSeen)
+if ((argx[0]) == '24') {
+ println "24: select S where s limit 1"
+ def temp = h.step24(idOfSentenceJustSeen)
  //temp == -1: no sentence found
  //temp  >  0: exactly one sentence found
  if (temp > 0) {
@@ -654,15 +468,19 @@ if ((argx[0]) == '17') {
  h.step17(idOfSentenceJustSeen, regexp)
 }
 
-if ((argx[0]) == '18') {
+if ((argx[0]) =~ '18') {
  println "18: select S where s and r limit 1 {regexp}"
+ if (argx.size() > 1) {
  String regexp = argx[1]
- def temp = h.step18(idOfSentenceJustSeen, regexp)
+ boolean printSQL = (argx[0] =~ 'q')
+ def temp = h.step18(printSQL, idOfSentenceJustSeen, regexp)
  //temp == -1: no sentence found
  //temp  >  0: exactly one sentence found
  if (temp > 0) {
   idOfSentenceJustSeen = temp
  }
+ }
+ else println "?"
 }
 
 if ((argx[0]) == '19') {
@@ -710,7 +528,7 @@ if ((argx[0]) == '0') {
  println "19: select S where r {regexp}"
  println "20: select S where r limit 1 {regexp}"
  println "16: select S where s {}"
- println "16b select S where s limit 1 {}"
+ println "24: select S where s limit 1 {}"
  println "17: select S where s and r {regexp}"
  println "18: select S where s and r limit 1 {regexp}"
  println "21: select s where s {}"
