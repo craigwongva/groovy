52687cd9465456feec1559e06fc8de7830413f8d
EC2 Default User <ec2-user@ip-172-31-22-135.us-west-2.compute.internal>
2017-04-10 20:54:57 +0000

    Detect S3BlobStore userids

--- upload.groovy
+++ upload.groovy
@@ -27,38 +27,61 @@ import com.amazonaws.services.s3.AmazonS3Client.*;
 //  -Daws.accessKeyId=REDACTED -Daws.secretKey=REDACTED \
 //   upload/upload
 
-//This code is borrowed from
-//  http://docs.aws.amazon.com/AmazonS3/latest/dev/UploadObjSingleOpJava.html
-
-//Here is the CLI command that is somewhat equivalent
-// to the following code:
-//  aws s3 cp craigdeleteme s3://s3-encryption-dgr
-//   --region us-east-1 --sse-kms-key-id alias/craigkey --sse aws:kms
 public class UploadObjectSingleOperation {
 
-    def createFiveKeys() {
+    def createFiveKeys(boolean createKeys) {
 
-        def spaceEncryptionkey = [
-      	    'test': '9d756117-08ce-4426-9f88-160576b6325e,piazza-kms-test,gsn-iam-test-S3BlobstoreUser-14JQDCWOCNXRU',
-            'dev':  '90a294e3-13c0-47ba-bf08-6f88f286de84,piazza-kms-dev,gsn-iam-dev-S3BlobstoreUser-1OM0QYZL8BTEP',
-            'int':  '42de4feb-844f-43d3-bff9-6bde53474aff,piazza-kms-int,gsn-iam-int-S3BlobstoreUser-W4XRSOXFO9VF',
-            'stage':'bcdebfc2-68d1-4473-8675-73da960326e1,piazza-kms-stage,gsn-iam-stage-S3BlobstoreUser-MIF4H60PGOSX',
-            'prod': '02291128-c6ab-46bf-a918-c989fb6e2c71,piazza-kms-prod,gsp-iam-prod-S3BlobstoreUser-1056HWJ5M3PXS'
-        ]
-    
+	def spaces = [
+	    'test', 'dev', 'int', 'stage', 'prod'
+	]
+
+	def putKeyPolicy = [:]
+
+	def spaceEncryptionkey = [:]
+	//when loaded will look like this:
+        // [
+      	//  'test': 'gsn-iam-test-S3BlobstoreUser-14JQDCWOCNXRU',
+        //  'dev':  'gsn-iam-dev-S3BlobstoreUser-1OM0QYZL8BTEP',
+        //  'int':  'gsn-iam-int-S3BlobstoreUser-W4XRSOXFO9VF',
+        //  'stage':'gsn-iam-stage-S3BlobstoreUser-MIF4H60PGOSX',
+        //  'prod': 'gsp-iam-prod-S3BlobstoreUser-1056HWJ5M3PXS'
+        // ]
+
+	spaces.each {
+	    def s1 = "aws iam list-users --output text".execute() | "grep S3BlobstoreUser".execute() | "grep $it".execute()
+	    def t1 = s1.text.split()
+	    spaceEncryptionkey[it] = t1[5]
+	}
+
+	def existingKeys = [:]
+
+ 	if (createKeys) {
+	    spaces.each {
+                def s1 = "aws kms create-key --description $alias --region us-east-1 --output text".execute()
+                def t1 = s1.text.split()
+                def keyId = t1[6] 
+		existingKeys[it] = keyId
+	    }
+	}   
+	else {
+	    existingKeys = [
+      	        'test': '9d756117-08ce-4426-9f88-160576b6325e',
+                'dev':  '90a294e3-13c0-47ba-bf08-6f88f286de84',
+                'int':  '42de4feb-844f-43d3-bff9-6bde53474aff',
+                'stage':'bcdebfc2-68d1-4473-8675-73da960326e1',
+                'prod': '02291128-c6ab-46bf-a918-c989fb6e2c71'
+	    ]
+	}
+
+	spaces.each { k ->
+	    spaceEncryptionkey[k] = existingKeys[k] + ',' + spaceEncryptionkey[k]
+	}
+
         spaceEncryptionkey.each { k, v ->
             def keyId = v.split(',')[0]
-            def alias = v.split(',')[1]
-            def userid = v.split(',')[2]
+            def userid = v.split(',')[1]
+            def alias = 'piazza-kms-' + k
            
-            //AWS KMS enforces a 7 day waiting period before deletion, so
-            // the next five lines are commented out to prevent creation of new keys.
-            //def s1 = "aws kms create-key --description $alias --region us-east-1 --output text".execute()
-            //def t1 = s1.text.split()
-            //def keyId = t1[6] 
-            
-            println "I just pretend created $keyId"
-            
             def s2 = "aws kms create-alias --alias-name alias/$alias --target-key-id $keyId --region us-east-1".execute()
             println ""
             println "create-alias err.text: ${s2.err.text}"
@@ -66,15 +89,27 @@ public class UploadObjectSingleOperation {
             
             def s3 = 'aws kms put-key-policy --key-id ' + keyId + ' --region us-east-1 --policy-name default --policy \'{"Version":"2012-10-17","Id":"key-consolepolicy-3","Statement":[{"Sid":"EnableIAMUserPermissions","Effect":"Allow","Principal":{"AWS":"arn:aws:iam::539674021708:root"},"Action":"kms:*","Resource":"*"},{"Sid":"AllowAccessForKeyAdministrators","Effect":"Allow","Principal":{"AWS":"arn:aws:iam::539674021708:user/cwong"},"Action":["kms:Create*","kms:Describe*","kms:Enable*","kms:List*","kms:Put*","kms:Update*","kms:Revoke*","kms:Disable*","kms:Get*","kms:Delete*","kms:TagResource","kms:UntagResource","kms:ScheduleKeyDeletion","kms:CancelKeyDeletion"],"Resource":"*"},{"Sid":"AllowUseOfTheKey","Effect":"Allow","Principal":{"AWS":["arn:aws:iam::539674021708:user/' + userid + '"]},"Action":["kms:Encrypt","kms:Decrypt","kms:ReEncrypt*","kms:GenerateDataKey*","kms:DescribeKey"],"Resource":"*"},{"Sid":"AllowAttachmentOfPersistentResources","Effect":"Allow","Principal":{"AWS":["arn:aws:iam::539674021708:user/' + userid + '"]},"Action":["kms:CreateGrant","kms:ListGrants","kms:RevokeGrant"],"Resource":"*","Condition":{"Bool":{"kms:GrantIsForAWSResource":"true"}}}]}\' '
             
-           	//This script isn't able to submit the put-key-policy command.
-            // The problem is probably related to Groovy's handling of embedded
-            // whitespace in .execute() command strings.
-            println "\nRun the following manually from the command line for $k:\n"
-            println s3
-            println ""
+	    putKeyPolicy[k] = s3
         }
+
+        //This script isn't able to submit the put-key-policy command.
+        // The problem is probably related to Groovy's handling of embedded
+        // whitespace in .execute() command strings.
+	def f = new File('upload-puts')
+	putKeyPolicy.each { k, v ->
+            f << "${putKeyPolicy[k]}\n"
+	}
+        println "\nRun the following manually from the command line:\n"
+	println "./upload-puts"
     }
 
+    //This code is borrowed from
+    //  http://docs.aws.amazon.com/AmazonS3/latest/dev/UploadObjSingleOpJava.html
+
+    //Here is the CLI command that is somewhat equivalent
+    // to the following code:
+    //  aws s3 cp craigdeleteme s3://s3-encryption-dgr
+    //   --region us-east-1 --sse-kms-key-id alias/craigkey --sse aws:kms
     def upload() {
 	//Use BasicAWSCredentials (if you're going to hardcode the password (yuck)):
 	// BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAREDACTED ", "REDACTED")
@@ -121,5 +156,10 @@ public class UploadObjectSingleOperation {
 }
 
 def u = new UploadObjectSingleOperation()
-//u.createFiveKeys()
-u.upload()
+
+//AWS KMS enforces a 7 day waiting period before deletion, so
+// in order to prevent an overabundance of test keys, set to false:
+boolean CREATE_KEYS = false
+u.createFiveKeys(CREATE_KEYS)
+
+//u.upload()
