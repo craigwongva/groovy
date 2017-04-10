d77256a3fabf510c2a0f5baa717fc7b1ff545c6e
EC2 Default User <ec2-user@ip-172-31-22-135.us-west-2.compute.internal>
2017-04-10 19:33:00 +0000

    Add KMS code to create five keys

--- upload.groovy
+++ upload.groovy
@@ -22,7 +22,7 @@ import com.amazonaws.services.s3.AmazonS3Client.*;
 // JARS4=httpclient-4.3.6.jar:httpcore-4.3.3.jar
 // JARS5=jackson-annotations-2.5.0.jar:jackson-core-2.5.3.jar:jackson-databind-2.5.3.jar
 // JARS6=joda-time-2.8.1.jar
-// JARS=$JARS1:$JARS2:$JARS3:$JARS4:$JARS5:$JARS6
+// JARS=$JARS1:$JARS2:$JARS3:$JARS4:$JARS5:$JARS6 
 // java -cp $JARS \
 //  -Daws.accessKeyId=REDACTED -Daws.secretKey=REDACTED \
 //   upload/upload
@@ -36,6 +36,45 @@ import com.amazonaws.services.s3.AmazonS3Client.*;
 //   --region us-east-1 --sse-kms-key-id alias/craigkey --sse aws:kms
 public class UploadObjectSingleOperation {
 
+    def createFiveKeys() {
+
+        def spaceEncryptionkey = [
+      	    'test': '9d756117-08ce-4426-9f88-160576b6325e,piazza-kms-test,gsn-iam-test-S3BlobstoreUser-14JQDCWOCNXRU',
+            'dev':  '90a294e3-13c0-47ba-bf08-6f88f286de84,piazza-kms-dev,gsn-iam-dev-S3BlobstoreUser-1OM0QYZL8BTEP',
+            'int':  '42de4feb-844f-43d3-bff9-6bde53474aff,piazza-kms-int,gsn-iam-int-S3BlobstoreUser-W4XRSOXFO9VF',
+            'stage':'bcdebfc2-68d1-4473-8675-73da960326e1,piazza-kms-stage,gsn-iam-stage-S3BlobstoreUser-MIF4H60PGOSX',
+            'prod': '02291128-c6ab-46bf-a918-c989fb6e2c71,piazza-kms-prod,gsp-iam-prod-S3BlobstoreUser-1056HWJ5M3PXS'
+        ]
+    
+        spaceEncryptionkey.each { k, v ->
+            def keyId = v.split(',')[0]
+            def alias = v.split(',')[1]
+            def userid = v.split(',')[2]
+           
+            //AWS KMS enforces a 7 day waiting period before deletion, so
+            // the next five lines are commented out to prevent creation of new keys.
+            //def s1 = "aws kms create-key --description $alias --region us-east-1 --output text".execute()
+            //def t1 = s1.text.split()
+            //def keyId = t1[6] 
+            
+            println "I just pretend created $keyId"
+            
+            def s2 = "aws kms create-alias --alias-name alias/$alias --target-key-id $keyId --region us-east-1".execute()
+            println ""
+            println "create-alias err.text: ${s2.err.text}"
+            println "create-alias    .text: ${s2.text}"
+            
+            def s3 = 'aws kms put-key-policy --key-id ' + keyId + ' --region us-east-1 --policy-name default --policy \'{"Version":"2012-10-17","Id":"key-consolepolicy-3","Statement":[{"Sid":"EnableIAMUserPermissions","Effect":"Allow","Principal":{"AWS":"arn:aws:iam::539674021708:root"},"Action":"kms:*","Resource":"*"},{"Sid":"AllowAccessForKeyAdministrators","Effect":"Allow","Principal":{"AWS":"arn:aws:iam::539674021708:user/cwong"},"Action":["kms:Create*","kms:Describe*","kms:Enable*","kms:List*","kms:Put*","kms:Update*","kms:Revoke*","kms:Disable*","kms:Get*","kms:Delete*","kms:TagResource","kms:UntagResource","kms:ScheduleKeyDeletion","kms:CancelKeyDeletion"],"Resource":"*"},{"Sid":"AllowUseOfTheKey","Effect":"Allow","Principal":{"AWS":["arn:aws:iam::539674021708:user/' + userid + '"]},"Action":["kms:Encrypt","kms:Decrypt","kms:ReEncrypt*","kms:GenerateDataKey*","kms:DescribeKey"],"Resource":"*"},{"Sid":"AllowAttachmentOfPersistentResources","Effect":"Allow","Principal":{"AWS":["arn:aws:iam::539674021708:user/' + userid + '"]},"Action":["kms:CreateGrant","kms:ListGrants","kms:RevokeGrant"],"Resource":"*","Condition":{"Bool":{"kms:GrantIsForAWSResource":"true"}}}]}\' '
+            
+           	//This script isn't able to submit the put-key-policy command.
+            // The problem is probably related to Groovy's handling of embedded
+            // whitespace in .execute() command strings.
+            println "\nRun the following manually from the command line for $k:\n"
+            println s3
+            println ""
+        }
+    }
+
     def upload() {
 	//Use BasicAWSCredentials (if you're going to hardcode the password (yuck)):
 	// BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAREDACTED ", "REDACTED")
@@ -44,12 +83,10 @@ public class UploadObjectSingleOperation {
 	def awsCreds = new SystemPropertiesCredentialsProvider().getCredentials() 
 
 	Region usEast1 = Region.getRegion(Regions.US_EAST_1);
-	def bucketName     = "s3-encryption-dgr";
-	def keyName        = "craigdeleteme221"; //the target object name in S3
-	def uploadFileName = "craigdeleteme222"; //the source file name in EC2 instance
-	def keyID          = "alias/craigkey"    //aws kms create-key --description craig --region us-east-1
-						 //aws kms create-alias --alias-name alias/craigkey 
-						 // --target-key-id <value> --region us-east-1
+	def bucketName     = "s3-encryption-dgr"
+	def keyName        = "craigdeleteme221" //the target object name in S3
+	def uploadFileName = "craigdeleteme222" //the source file name in EC2 instance
+	def keyID          = "alias/piazza-kms-test"
 
         def s3client = new AmazonS3Client(awsCreds)
 	s3client.setRegion(usEast1);
@@ -84,4 +121,5 @@ public class UploadObjectSingleOperation {
 }
 
 def u = new UploadObjectSingleOperation()
+//u.createFiveKeys()
 u.upload()
