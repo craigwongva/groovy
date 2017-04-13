c736dab3fc8432b5bb57a31b30b2bad38725436a
EC2 Default User <ec2-user@ip-172-31-22-135.us-west-2.compute.internal>
2017-04-13 18:18:38 +0000

    Refactor uupsPzblobstore

--- upload.groovy
+++ upload.groovy
@@ -24,131 +24,178 @@ import com.amazonaws.services.s3.AmazonS3Client.*;
 // JARS5=jackson-annotations-2.5.0.jar:jackson-core-2.5.3.jar:jackson-databind-2.5.3.jar
 // JARS6=joda-time-2.8.1.jar
 // JARS=$JARS1:$JARS2:$JARS3:$JARS4:$JARS5:$JARS6 
-// java -cp $JARS \
-//  -Daws.accessKeyId=REDACTED -Daws.secretKey=REDACTED \
-//   upload/upload
 //
 //To run this code (for the createOneEncryptionKeyPerPCFInstance method):
 // java -cp $JARS \
-//   upload/upload createOneEncryptionKeyPerPCFInstance <buildKeys> <encryptionKeyId> <encryptionKeyDescription>
+//   upload/upload createOneEncryptionKeyPerPCFInstance 
+//    <buildKeys> <encryptionKeyId> <encryptionKeyDescription>
 //where <buildKeys> is true or false
 //If <buildKeys> is true, then <encryptionKeyId> is ignored
 //Note 1: encryptionKeyDescription looks like 'x-y-l2'
 //Note 2: AWS KMS enforces a 7 day waiting period before deletion, so
-// in order to prevent an overabundance of test keys, set to false:
+// in order to prevent an overabundance of test keys, set to false.
 //
 //To run this code (for the uups method):
 // java -cp $JARS \
 //   upload/upload <cfuser> <cfpassword> <cfspace> <encryptionKeyAlias>
-//Note: encryptionKeyAlias must include the prefix 'alias/'
+//Note 1: encryptionKeyAlias must include the prefix 'alias/'
+//Note 2: An empty list of apps is a problem because
+// at least one app is needed to get the pz-blobstore
+// credentials (because there is a 'cf cups' and
+// 'cf uups' but no 'cf list-user-provided-service').
+//
+//To run this code (for the upload+encrypt demo):
+// java -cp $JARS \
+//  -Daws.accessKeyId=REDACTED -Daws.secretKey=REDACTED \
+//   upload/upload
+//Note 1: The two redacted values are passed in via
+// -D parameters, not within args[]
 
 public class UploadObjectSingleOperation {
 
-/*
-Steps for updating pz-blobstore:
-- identify all apps that use pz-blobstore
-- for the first app, do ‘./cf env <appfullname>’
-- find the pz-blobstore credentials
-- write them plus the encryption_key into a .json file
-- uups
-- restage each app that uses pz-blobstore
-*/
-    def uups(args) {
-	def (dummy, user, pwd, cfspace, encryptionkey) = args
+    def uupsPzblobstore(args) {
+	def (
+	    ignore,		//name of method to invoke
+            user, 		//PCF userid that can uups pz-blobstore
+            pwd, 		//
+            cfspace, 		//method only updates one space's pz-blobstore
+            encryptionkey	//do include 'alias/', e.g. 'alias/zzz-zzz-zzz'
+        ) = args
 
-        def cfcmd = '/home/ec2-user/groovy/cf'
+        String cfcmd = '/home/ec2-user/groovy/cf'
+
+	cflogin(cfcmd, user, pwd, cfspace)
+
+        ArrayList appsUsingBlobstore = getAppsUsingBlobstore(cfcmd)
+	if (appsUsingBlobstore.size() == 0) {
+	    println "Error: At least one app must use pz-blobstore."
+	    return
+	}
+
+        getUpsCredentialsAndWriteToJson(
+            cfcmd, appsUsingBlobstore, encryptionkey)
+
+        uupsViaJson(cfcmd, cfspace)
+
+        restageAppsUsingBlobstore(cfcmd, cfspace, appsUsingBlobstore)
+
+        cflogout(cfcmd)
+    }
+
+    void cflogin(String cfcmd, String user, String pwd, String cfspace) {
         def cftarget = 'https://api.devops.geointservices.io'
         def cfskip = '--skip-ssl-validation'
         def cforg = 'piazza'
-        def cmd = "$cfcmd login -a $cftarget -u $user -p $pwd -o $cforg -s $cfspace $cfskip"
-        def cmdtext = cmd.execute()
-	println cmdtext.err.text
-	println cmdtext.text
 
-	def s1 = "$cfcmd services".execute() | "grep pz-blobstore".execute()
-	def t1 = s1.text.split()
+        def s = "$cfcmd login -a $cftarget "
+        s += "-u $user -p $pwd -o $cforg -s $cfspace $cfskip"
+
+        def sx = s.execute()
+	println sx.err.text
+	println sx.text
+    }
+
+	ArrayList getAppsUsingBlobstore(String cfcmd) {
+	    def s1 = "$cfcmd services".execute() | "grep pz-blobstore".execute()
+	    def t1 = s1.text.split()
 	
-	ArrayList appsUsingBlobstore = []
-	for (int i=2; i<t1.size(); i++) {
-	    //sometimes the split() returns an empty string
-	    if (t1[i] != '') {
-		appsUsingBlobstore.add(t1[i].replaceAll(',', ''))
+	    def a = []
+	    for (int i=2; i<t1.size(); i++) {
+	        //sometimes the split() returns an empty string
+	        if (t1[i] != '') {
+		    a.add(t1[i].replaceAll(',', ''))
+	        }
 	    }
+            a
 	}
 
-	if (appsUsingBlobstore.size() == 0) {
-	    //An empty list of apps is a problem because
-	    // at least one app is needed to get the pz-blobstore
-	    // credentials (because there is a 'cf cups' and
-	    // 'cf uups' but no 'cf list-user-provided-service').
-	    println "Error: The list of apps using pz-blobstore is empty."
-	    return
-	}
+        HashMap cfenv(String cfcmd, ArrayList appsUsingBlobstore) {
+	    String arbitraryApp = appsUsingBlobstore[0]
 
-	println "About to use 'cf env' to get pz-blobstore credentials"
-	def arbitraryApp = appsUsingBlobstore[0]
+            def cmd3 = "$cfcmd env $arbitraryApp"
+            def cmdtext3 = cmd3.execute()
+	    println cmdtext3.err.text
 
-        def cmd3 = "$cfcmd env $arbitraryApp"
-        def cmdtext3 = cmd3.execute()
-	println cmdtext3.err.text
-	def lines3 = cmdtext3.text.split('\n')
-	int lineContainingAppblobstore
-	for (int i=0; i<lines3.size(); i++) {
-	    if (lines3[i] =~ /app-blobstore/) {
-		lineContainingAppblobstore = i
+	    def lines3 = cmdtext3.text.split('\n')
+	    int lineContainingAppblobstore
+	    for (int i=0; i<lines3.size(); i++) {
+	        if (lines3[i] =~ /app-blobstore/) {
+		    lineContainingAppblobstore = i
+	        }
 	    }
-	}
 
-	def credentials = [:]
-
-	if ((lineContainingAppblobstore < 5) ||
- 	    (lineContainingAppblobstore > lines3.size() - 5)) {
-	    println "Error: Unexpected 'cf env' output format."
-	    return
-	}
-
-	for (int i=lineContainingAppblobstore-5; 
+	    def credentials = [:]
+	    if ((lineContainingAppblobstore < 5) ||
+ 	        (lineContainingAppblobstore > lines3.size() - 5)) {
+	        println "Error: Unexpected 'cf env' output format."
+	        return
+	    }
+	    for (int i=lineContainingAppblobstore-5; 
 		 i<=lineContainingAppblobstore+5; i++) {
-	    String withoutComma = lines3[i].replaceAll(',', '')
-	    [
-	     //These are the expected four credential variables
-	     // for pz-blobstore. If new ones are introduced
-	     // they must be added to this list, because
-	     // this code does not interpret the 'cf env' json.
-	     'access_key_id', 
-	     'bucket', 
-	     'path',
-	     'secret_access_key'
-	    ].each {
-	        if (lines3[i] =~ it) credentials[it] = withoutComma
+	        String withoutComma = lines3[i].replaceAll(',', '')
+	        [
+	         //These are the expected four credential variables
+	         // for pz-blobstore. If new ones are introduced
+	         // they must be added to this list, because
+	         // this code does not interpret the 'cf env' json.
+	         'access_key_id', 
+	         'bucket', 
+	         'path',
+	         'secret_access_key'
+	        ].each {
+	            if (lines3[i] =~ it) credentials[it] = withoutComma
+	        }
 	    }
-	}
-	credentials['encryption_key'] = '     "encryption_key": "' + encryptionkey + '"'
+            credentials
+        }
 
-	println "About to write to uups json..."
-	def f = new File('upload-uups.json')
-	f.write "{\n"
-	f << "${credentials['access_key_id']},\n"
-	f << "${credentials['bucket']},\n"
-	f << "${credentials['encryption_key']},\n"
-	f << "${credentials['path']},\n"
-	f << "${credentials['secret_access_key']}\n"
-	f << "}\n"
+        void writeToUupsJson(HashMap credentials) {
+	    println "About to write to uups json..."
+	    def f = new File('upload-uups.json')
+	    f.write "{\n"
+	    f << "${credentials['access_key_id']},\n"
+	    f << "${credentials['bucket']},\n"
+	    f << "${credentials['encryption_key']},\n"
+	    f << "${credentials['path']},\n"
+	    f << "${credentials['secret_access_key']}\n"
+	    f << "}\n"
+        }
 
-	println "About to uups pz-blobstore in $cfspace..."
+        void getUpsCredentialsAndWriteToJson(
+            String cfcmd, ArrayList appsUsingBlobstore,
+            String encryptionkey) {
+
+            HashMap upsCredentials = cfenv(cfcmd, appsUsingBlobstore)
+	    upsCredentials['encryption_key'] = 
+                '     "encryption_key": "' + encryptionkey + '"'
+
+            writeToUupsJson(upsCredentials) 
+        }
+
+    void uupsViaJson(String cfcmd, String cfspace) {
+        println "About to uups pz-blobstore in $cfspace..."
         def cmd4 = "$cfcmd uups pz-blobstore -p upload-uups.json"
         def cmdtext4 = cmd4.execute()
 	println cmdtext4.err.text
 	println cmdtext4.text
+    }
+
+    void restageAppsUsingBlobstore(
+        String cfcmd, String cfspace, 
+        ArrayList appsUsingBlobstore) {
 
 	appsUsingBlobstore.each {
 	    println "About to restage $cfspace $it"
             def cmd5 = "$cfcmd restage $it"
+/* turn off while refactoring
             def cmdtext5 = cmd5.execute()
 	    println cmdtext5.err.text
 	    println cmdtext5.text
+*/
 	}
+    }
 
+    void cflogout(String cfcmd) {
         def cmd2 = "$cfcmd logout"
         def cmdtext2 = cmd2.execute()
 	println cmdtext2.err.text
@@ -217,15 +264,15 @@ Steps for updating pz-blobstore:
         String encryptionKeyAlias, String encryptionKeyId, 
         String region) {
 
-        def s1 = 'aws kms create-alias '
-	s1 += "--alias-name alias/$encryptionKeyAlias "
-	s1 += "--target-key-id $encryptionKeyId "
-	s1 += "--region $region"
-        def s1x = s1.execute()
+        def s = 'aws kms create-alias '
+	s += "--alias-name alias/$encryptionKeyAlias "
+	s += "--target-key-id $encryptionKeyId "
+	s += "--region $region"
+        def sx = s.execute()
         println ""
-	println s1
-        println "create-alias err.text: ${s1x.err.text}"
-        println "create-alias    .text: ${s1x.text}"
+	println s
+        println "create-alias err.text: ${sx.err.text}"
+        println "create-alias    .text: ${sx.text}"
     }
 
     String buildPolicyJSON(
@@ -286,13 +333,7 @@ Steps for updating pz-blobstore:
     //  aws s3 cp craigdeleteme s3://s3-encryption-dgr
     //   --region us-east-1 --sse-kms-key-id alias/craigkey --sse aws:kms
     def upload(args) {
-	//Use BasicAWSCredentials 
-	// (if you're going to hardcode the password (yuck)):
-	// BasicAWSCredentials awsCreds = 
-	//  new BasicAWSCredentials("AKIAREDACTED ", "REDACTED")
-
-	//Use SystemPropertiesCredentialsProvider 
-	// (if you're going to use -D command line parms):
+	//interpret -D command line parms:
 	def awsCreds = 
 	  new SystemPropertiesCredentialsProvider().getCredentials() 
 
@@ -334,4 +375,6 @@ Steps for updating pz-blobstore:
     }
 }
 
+//The following statement executes the method 
+// passed in as the args[0] parameter.
 new UploadObjectSingleOperation().{args[0]}(args)
